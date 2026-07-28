50dd9578944c2019d59bb73752aea46c174ebc82
Mhmd Rayan <m7mddotm7@gmail.com>
2026-07-28 14:20:07 +0300

    Add :a break entrance test

--- tests.py
+++ tests.py
@@ -30,6 +30,20 @@ class Tests(unittest.TestCase):
 
         self.assertEqual(len(maze._Maze__cells), num_cols)
         self.assertEqual(len(maze._Maze__cells[0]), num_rows)
+    
+    def test_break_entrance_and_exit(self):
+        num_cols = 4
+        num_rows = 3
+
+        maze = Maze(0, 0, num_rows, num_cols, 10, 10)
+
+        maze._Maze__break_entrance_and_exit()
+
+        top_left_cell = maze._Maze__cells[0][0]
+        bottom_right_cell = maze._Maze__cells[num_cols - 1][num_rows - 1]
+
+        self.assertEqual(top_left_cell.has_top_wall, False)
+        self.assertEqual(bottom_right_cell.has_bottom_wall, False)
 
 
 if __name__ == "__main__":
