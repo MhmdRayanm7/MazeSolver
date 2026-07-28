64d3a1b08b66b93344d5cf47b3fa9c093447987c
Mhmd Rayan <m7mddotm7@gmail.com>
2026-07-28 13:25:19 +0300

    fix : refactor the cell code

--- cell.py
+++ cell.py
@@ -16,34 +16,26 @@ class Cell:
     def draw(self, x1: float, y1: float, x2: float, y2: float) -> None:
         if self.__win is None:
             return
-        
         self.__x1 = x1
         self.__x2 = x2
         self.__y1 = y1
         self.__y2 = y2
-        
         if self.has_left_wall:
             line = Line(Point(x1, y1), Point(x1, y2))
-        if self.__win is not None:
             self.__win.draw_line(line)
-            
         if self.has_top_wall:
             line = Line(Point(x1, y1), Point(x2, y1))
-        if self.__win is not None:
             self.__win.draw_line(line)
-            
         if self.has_right_wall:
             line = Line(Point(x2, y1), Point(x2, y2))
-        if self.__win is not None:
             self.__win.draw_line(line)
-            
         if self.has_bottom_wall:
             line = Line(Point(x1, y2), Point(x2, y2))
-        if self.__win is not None:
             self.__win.draw_line(line)
-    
+
     def draw_move(self, to_cell: "Cell", undo: bool = False) -> None:
-        
+        if self.__win is None:
+            return
         half_length = abs(self.__x2 - self.__x1) // 2
         x_center = half_length + self.__x1
         y_center = half_length + self.__y1
@@ -57,5 +49,4 @@ class Cell:
             fill_color = "gray"
 
         line = Line(Point(x_center, y_center), Point(x_center2, y_center2))
-        if self.__win is not None:
-            self.__win.draw_line(line, fill_color)
+        self.__win.draw_line(line, fill_color)
